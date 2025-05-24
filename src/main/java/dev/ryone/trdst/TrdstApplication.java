1489656e61a2c5d0497bce240510755c01008016
ryone9re <ryoichi.kurimoto@gmail.com>
2025-05-25 08:54:35 +0900

    Clear

--- src/main/java/dev/ryone/trdst/TrdstApplication.java
+++ src/main/java/dev/ryone/trdst/TrdstApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrdstApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TrdstApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TrdstApplication.class, args);
+    }
 }
